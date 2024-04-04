6f0d2a22ea8e451beeaf81656f4fd0fa2158ab52
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-04 14:19:23 +0530

    Add game logic to handle player moves

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 function Game() {
+
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {}
+
   return (
     <div className="game">
       <div className="game-board">
