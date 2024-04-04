55cbafb12092951c37d5df61606f4c10341b678e
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-04 12:47:13 +0530

    Refactor handleClick function and add calculateWinner function

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(index) {
-    if(squares[index]) {
+    if(squares[index] || calculateWinner(squares)) {
       return;
     }
     const newSquares = squares.slice();
@@ -56,4 +56,27 @@ Square.propTypes = {
   value: PropTypes.any,
   onSquareClick: PropTypes.func,
 };
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], // 1st row
+    [3, 4, 5], // 2nd row
+    [6, 7, 8], // 3rd row
+    [0, 3, 6], // 1st column
+    [1, 4, 7], // 2nd column
+    [2, 5, 8], // 3rd column
+    [0, 4, 8], // 1st diagonal
+    [2, 4, 6], // 2nd diagonal
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
 export default Board;
