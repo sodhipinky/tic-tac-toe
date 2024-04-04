59a9ab4e2b447f17f8f8cf71e0a23efe99997727
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-04 12:24:54 +0530

    Refactor Square component to accept value and onClick prop

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,47 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick() {
+    const newSquares = squares.slice();
+    newSquares[0] = 'X';
+    setSquares(newSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   )
 }
 
-function Square() {
-
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-
+function Square({value, onSquareClick}) {
   return (
     <button
-    className="square"
-    onClick={ handleClick }
-    >{ value }</button>
+      className="square"
+      onClick={onSquareClick}>
+      {value}
+    </button>
   )
 }
 
 Square.propTypes = {
-   value: PropTypes.any,
+  value: PropTypes.any,
 };
 export default Board;
