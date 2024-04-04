20f00a3b2daca5dcaf1fbab9173689e733989944
Pinky Sodhi <pinky.sodhi@gmail.com>
2024-04-04 12:01:34 +0530

    Refactor board layout and add state to Square component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   )
 }
 
-function Square({ value }) {
+function Square() {
+
+  const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log('click');
+    setValue('X');
   }
 
   return (
